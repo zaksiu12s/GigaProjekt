10dd70a476a3f4ccaf3d23469d5834beec073740
Maks Zajac <129377563+zaksiu12s@users.noreply.github.com>
2024-10-22 15:44:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ function updateData() {
     const hourCountContainer = document.querySelector("[data-id='hourContainer']").querySelector("td");
     const minuteCountContainer = document.querySelector("[data-id='minuteContainer']").querySelector("td");
     const secondCountContainer = document.querySelector("[data-id='secondContainer']").querySelector("td");
-    const milisecondCountContainer = document.querySelector("[data-id='milisecondContainer']").querySelector("td");
-
+    
     const currentDate = new Date();
     const incidentDate = currentDate.getTime();
     const diff = incidentDate - 1728843904000;
@@ -15,8 +14,6 @@ function updateData() {
     hourCountContainer.innerText = (Math.round(diff / 1000 / 60 / 60 * 100) / 100).toFixed(2);
     minuteCountContainer.innerText = (Math.round((diff / 1000 / 60) * 100) / 100).toFixed(2);
     secondCountContainer.innerText = (Math.round((diff / 1000) * 1000) / 1000).toFixed(3);
-    milisecondCountContainer.innerText = diff;
-
     return setInterval(updateData, 1000);
 }
 
